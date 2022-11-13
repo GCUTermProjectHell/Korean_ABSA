01ac4c9913d2251e7101f91f129e75e8d1596f92
Kim Y.C <87708360+Kimy19@users.noreply.github.com>
2022-11-14 04:28:23 +0900

    Update inference.sh

--- models/kyElectra/inference.sh
+++ models/kyElectra/inference.sh
@@ -4,36 +4,40 @@
 cd src
 echo run task: Inference entity_property model  !!
 python ky_ACD.py \
-  --base_model kimy1119/gcu_ky1\
+  --base_model kykim/electra-kor-base\
   --do_test \
+  --entity_property_model_path '../../../saved_model/kyElectra/gcu_ky1.pt' \
   --save_path '../../../saved_result/kyElectra/gcu_ky1.json'\
   --classifier_hidden_size 768\
 
 echo run task: Inference entity_property model  !!
 python ky_ACD.py \
-  --base_model kimy1119/gcu_ky2\
+  --base_model kykim/electra-kor-base\
   --do_test \
+  --entity_property_model_path '../../../saved_model/kyElectra/gcu_ky2.pt' \
   --save_path '../../../saved_result/kyElectra/gcu_ky2.json'\
   --classifier_hidden_size 768\
 
 echo run task: Inference entity_property model  !!
 python ky_ACD.py \
-  --base_model kimy1119/gcu_ky3\
+  --base_model kykim/electra-kor-base\
   --do_test \
+  --entity_property_model_path '../../../saved_model/kyElectra/gcu_ky3.pt' \
   --save_path '../../../saved_result/kyElectra/gcu_ky3.json'\
   --classifier_hidden_size 768\
 
 echo run task: Inference entity_property model  !!
 python ky_ACD.py \
-  --base_model kimy1119/gcu_ky4\
+  --base_model kykim/electra-kor-base\
   --do_test \
+  --entity_property_model_path '../../../saved_model/kyElectra/gcu_ky4.pt' \
   --save_path '../../../saved_result/kyElectra/gcu_ky4.json'\
   --classifier_hidden_size 768\
 
 echo run task: Inference entity_property model  !!
 python ky_ACD.py \
-  --base_model kimy1119/gcu_ky5\
+  --base_model kykim/electra-kor-base\
   --do_test \
+  --entity_property_model_path '../../../saved_model/kyElectra/gcu_ky5.pt' \
   --save_path '../../../saved_result/kyElectra/gcu_ky5.json'\
   --classifier_hidden_size 768\
-
